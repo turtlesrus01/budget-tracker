326e52de71c2ecdc46b74ae1a9482538eaac7ad8
Green Camper <stefan.palacios@gmail.com>
2023-05-01 21:15:56 -0500

    Completed model/index code with 4 models included

--- models/index.js
+++ models/index.js
@@ -1,26 +1,38 @@
 //import model files
-const User = require('./User');
-const Expense = require('./Expense');
-const Category = require('./Category');
-const Goal = require('./Goal');
+const User = require("./User");
+const Expenses = require("./Expenses");
+const Category = require("./Category");
+const Goals = require("./Goals");
 
-//User => Expenses
-User.hasMany(Expense, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+//User can have many Expenses
+User.hasMany(Expenses, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
-//Expenses => User
-Expense.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+//Expenses have one User
+Expenses.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
-//Expenses => Categories
-User.hasOne(Expense, {
-  foreignKey: 'expense_id',
-  onDelete: 'CASCADE'
+//Each expense has one Category
+Expenses.hasOne(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
-//Category => Expenses
-//User => Goals
-//Goals => User
-
-module.exports = { User };
+//Category is related to Expenses
+Category.belongsTo(Expenses, {
+  foreignKey: "expense_id",
+  onDelete: "CASCADE",
+});
+//User can have many Goals
+User.hasMany(Goals, {
+  foreignKey: "goal_id",
+  onDelete: "CASCADE",
+});
+//Goals are tied to one User
+Goals.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+//export classes
+module.exports = { User, Category, Expense, Goals };
