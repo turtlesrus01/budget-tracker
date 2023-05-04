976d24702d3117105754c5a6409de386d2e16d7c
Green Camper <stefan.palacios@gmail.com>
2023-05-03 20:26:24 -0500

    Completed code for Goals and Category Model

--- models/Category.js
+++ models/Category.js
@@ -1,4 +1,33 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 const User = require("./User");
-const Category = require("./Category");
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
+
+module.exports = Category;
