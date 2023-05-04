77dccbd35137f9c3a1d2d16103c3838207b49333
Green Camper <stefan.palacios@gmail.com>
2023-05-03 20:42:34 -0500

    Added code for connection.js

--- models/Category.js
+++ models/Category.js
@@ -1,21 +1,23 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-const User = require("./User");
 
 class Category extends Model {}
 
 Category.init(
   {
+    //Category id
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+    //Category name
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    //Color of category
     color: {
       type: DataTypes.STRING,
       allowNull: false,
