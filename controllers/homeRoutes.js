05e361ec7b309131ea8451912cef141fcdfb48f6
Change Agent <keasiley@icloud.com>
2023-05-07 20:57:44 -0500

    homeroutes comments added back in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,24 +2,24 @@ const router = require('express').Router();
 const { User, Category } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findAll({
-//       attributes: { exclude: ['password'] },
-//       order: [['name', 'ASC']],
-//     });
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
 
-//     const users = userData.map((category) => category.get({ plain: true }));
+    const users = userData.map((category) => category.get({ plain: true }));
 
-//     res.render('homepage', {
-//       title: 'Homepage',
-//       users,
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('homepage', {
+      title: 'Homepage',
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
