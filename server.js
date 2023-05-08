62a87481583fd0897e052b8320b3c015b713c562
Green Camper <stefan.palacios@gmail.com>
2023-05-07 23:00:43 -0500

    Added 'morgan', server logging module

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection')
 //const helpers = require('./utils/helpers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+//server logging module
+const logger = require('morgan');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -37,6 +40,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+//logging module connection to express
+app.use(logger('dev'))
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
