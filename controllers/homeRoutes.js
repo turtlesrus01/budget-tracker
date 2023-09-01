afb5dce547c5488a7612017c322a7823cd0fc922
Green Camper <stefan.palacios@gmail.com>
2023-08-31 19:15:06 -0500

    Added auth to root endpoint

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,19 +1,21 @@
-const router = require('express').Router();
-const { User, Category } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Goals, Expenses } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', withAuth, async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    const userData = await User.findByPk(req.session.userId, {
+      attributes: {
+        exclude: ["password"],
+      },
+      include: [Goals, Expenses]
     });
 
-    const users = userData.map((category) => category.get({ plain: true }));
-
-    res.render('dashboard', {
-      title: 'Dashboard',
-      users,
+    const user = userData.get({ plain: true });
+    console.log(user);
+    res.render("dashboard", {
+      title: "Dashboard",
+      ...user,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -21,49 +23,49 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/dashboard');
+    res.redirect("/dashboard");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
-router.get('/signup', (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('signup');
-  });
-  
-  router.get('/logout', (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('logout');
-  });
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
 
-  router.get('/dashboard', (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('dashboard');
-  });
+  res.render("signup");
+});
 
-  router.get('/expenses', (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('expenses');
-  });
+router.get("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("logout");
+});
+
+router.get("/dashboard", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("dashboard");
+});
+
+router.get("/expenses", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("expenses");
+});
 
 module.exports = router;
