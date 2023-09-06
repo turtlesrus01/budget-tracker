e7578fc5c457464b883642896649a790c84815f7
Green Camper <stefan.palacios@gmail.com>
2023-09-05 21:07:53 -0500

    Added error logging GET route, correct status

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,17 @@
-const router = require('express').Router();
-const { User } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { User } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// Get route to catch errors
+router.get("/", async (req, res) => {
+  const userData = await User.findAll().catch((err) => {
+    res.json(err);
+  });
+  res.json(userData);
+});
 
 // User login
-router.post('/login', withAuth, async (req, res) => {
+router.post("/login", withAuth, async (req, res) => {
   try {
     //Query request for the user email
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -11,7 +19,7 @@ router.post('/login', withAuth, async (req, res) => {
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
     //Query request for the user email
@@ -20,24 +28,25 @@ router.post('/login', withAuth, async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: "Incorrect email or password, please try again" });
       return;
     }
     //Saving the session and changing logged_in state
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
 
+      res
+        .status(200)
+        .json({ user: userData, message: "You are now logged in!" });
+    });
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-// User logout 
-router.post('/logout', (req, res) => {
+// User logout
+router.post("/logout", (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
@@ -48,23 +57,20 @@ router.post('/logout', (req, res) => {
 });
 
 // User signup
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-try {
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+    });
 
-  const { name, email, password } = req.body;
-
-  const newUser = await User.create({
-    name,
-    email,
-    password,
-  });
-
-  res.status(200).json(newUser);
-} catch (err) {
-  
-  res.status(400).json(err);
-}
+    res.status(200).json(newUser);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
